e44210cf3fc1b5046e47dd3f15caac099170b818
ant0n0vskyi <vad-y-one@mail.ru>
2023-11-02 16:10:50 +0300

    ok

--- thanks_page.php
+++ thanks_page.php
@@ -22,7 +22,7 @@ if(mail($to, $subject, $message, $headers)) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <title>ElectroMaster Electrical Services</title>
+    <title>FitZone Studio</title>
 
     <style>
       * {
@@ -31,12 +31,12 @@ if(mail($to, $subject, $message, $headers)) {
       }
 
       .bodyClass1-fgZSxP4y {
-        background-color: #d36f22;
+        background-color: #9dc701;
         color: #ffffff;
       }
 
       .bodyClass1-fgZSxP4y .mainBlock-fgZSxP4y {
-        background: #8c4c1a;
+        background: #759504;
         color: #ffffff;
         border-left: 10px solid #fff4e3;
         border-right: 10px solid #fff4e3;
@@ -131,29 +131,29 @@ if(mail($to, $subject, $message, $headers)) {
       <div class="centerBlock">
         <div class="mainBlock-fgZSxP4y">
           <p>
-            Welcome to ElectroMaster, your trusted partner for electrical
-            installation and maintenance solutions. We appreciate your
-            confidence in ElectroMaster, and we're dedicated to providing
-            top-quality electrical services that go beyond your expectations.
+            Welcome to FitZone Studio, your ultimate destination for fitness
+            training and wellness solutions. We value your commitment to FitZone
+            Studio, and we are dedicated to offering unparalleled fitness
+            services that exceed your expectations.
           </p>
           <p>
-            Our team works tirelessly to ensure your satisfaction and a seamless
-            experience. We aim to light up your imagination and deliver lasting
-            results, helping you achieve your electrical goals.
+            Our team is passionately devoted to ensuring your well-being and a
+            seamless fitness journey. We strive to inspire your fitness goals
+            and provide enduring results, assisting you in realizing your
+            fitness aspirations.
           </p>
           <p>
-            Your feedback, comments, and ideas are essential for our continuous
-            improvement. If you have questions or insights to share, please
-            don't hesitate to get in touch. We're always ready to assist you in
-            crafting electrical solutions that meet your needs and preferences.
+            Your suggestions, feedback, and ideas are vital for our continual
+            enhancement. If you have any inquiries or insights to share, please
+            do not hesitate to reach out. We are always available to support you
+            in creating fitness solutions that cater to your requirements and
+            preferences.
           </p>
           <p>
-            Thank you for being a part of the ElectroMaster community. Your
-            trust fuels our commitment to electrical excellence.
+            Thank you for being a part of the FitZone Studio community. Your
+            trust motivates our dedication to fitness excellence.
           </p>
-          <p class="cBlock-fgZSxP4y">
-            Warm regards and heartfelt appreciation.
-          </p>
+          <p class="cBlock-fgZSxP4y">Warm regards and heartfelt gratitude.</p>
           <p class="cBlock-fgZSxP4y">
             <span class="domain-fgZSxP4y"></span>
           </p>
