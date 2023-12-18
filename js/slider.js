7141542efd55f615855cdb5de007d091440e1a1e
ant0n0vskyi <vad-y-one@mail.ru>
2023-12-19 02:55:19 +0300

    index

--- js/slider.js
+++ js/slider.js
@@ -25,3 +25,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
   setInterval(nextSlide, intervalTime);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".testimonials");
+  const slides = document.querySelector(".testimonials-cont");
+  let currentSlide = 0;
+  const intervalTime = 5000;
+
+  function updateSlider() {
+    const newTransformValue = -currentSlide * 100 + "%";
+    slides.style.transform = "translateX(" + newTransformValue + ")";
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.children.length;
+    updateSlider();
+  }
+
+  function prevSlide() {
+    currentSlide =
+      (currentSlide - 1 + slides.children.length) % slides.children.length;
+    updateSlider();
+  }
+
+  document
+    .querySelector(".testimonials-next")
+    .addEventListener("click", nextSlide);
+  document
+    .querySelector(".testimonials-prev")
+    .addEventListener("click", prevSlide);
+
+  setInterval(nextSlide, intervalTime);
+});
