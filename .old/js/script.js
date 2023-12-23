ae63bb3cb03ad53b7434915c105e4ab9cab97aac
ant0n0vskyi <vad-y-one@mail.ru>
2023-12-24 02:30:42 +0300

    finish

--- .old/js/script.js
+++
@@ -1,9 +0,0 @@
-const headerBurger = document.querySelector(".header_burger");
-const headerListMenu = document.querySelector(".menu_list");
-if (headerBurger) {
-  headerBurger.addEventListener("click", function (e) {
-    document.body.classList.toggle("_lock");
-    headerBurger.classList.toggle("_active");
-    headerListMenu.classList.toggle("_active");
-  });
-}
