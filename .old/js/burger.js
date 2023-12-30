ac3595c738734b1b8f1279d430c3f369579c5abd
ant0n0vskyi <vad-y-one@mail.ru>
2023-12-30 04:48:36 +0300

    ok

--- .old/js/burger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var menuButton = document.querySelector(".menu-button");
-  var navMenuWrapper = document.querySelector(".ujdgefj");
-
-  menuButton.addEventListener("click", function () {
-    navMenuWrapper.classList.toggle("show");
-  });
-});
--- js/gamburger.js
+++ js/gamburger.js
@@ -1,35 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var menuButton = document.querySelector('.menu-button');
-    var navOverlay = document.querySelector('.w-nav-overlay');
-    var menuItems = document.querySelectorAll('.nav-menu a');
-  
-    function openMenu() {
-      navOverlay.style.display = 'block';
-      document.addEventListener('click', closeMenuOutside);
-      window.addEventListener('hashchange', closeMenu);
+document.addEventListener("DOMContentLoaded", function () {
+  var menudjghcfd = document.querySelector(".menu-djghcfd");
+  var navOverlay = document.querySelector(".shdgsed-overlay");
+  var menuItems = document.querySelectorAll(".nav-menu a");
+
+  function openMenu() {
+    navOverlay.style.display = "block";
+    document.addEventListener("click", closeMenuOutside);
+    window.addEventListener("hashchange", closeMenu);
+  }
+
+  function closeMenu() {
+    navOverlay.style.display = "none";
+    document.removeEventListener("click", closeMenuOutside);
+    window.removeEventListener("hashchange", closeMenu);
+  }
+
+  function closeMenuOutside(event) {
+    if (
+      !navOverlay.contains(event.target) &&
+      !menudjghcfd.contains(event.target)
+    ) {
+      closeMenu();
     }
-  
-    function closeMenu() {
-      navOverlay.style.display = 'none';
-      document.removeEventListener('click', closeMenuOutside);
-      window.removeEventListener('hashchange', closeMenu);
-    }
-  
-    function closeMenuOutside(event) {
-      if (!navOverlay.contains(event.target) && !menuButton.contains(event.target)) {
-        closeMenu();
-      }
-    }
-  
-    menuButton.addEventListener('click', function (event) {
-      event.stopPropagation(); 
-      openMenu();
-    });
-  
-    menuItems.forEach(function (menuItem) {
-      menuItem.addEventListener('click', function (event) {
-        closeMenu();
-      });
+  }
+
+  menudjghcfd.addEventListener("click", function (event) {
+    event.stopPropagation();
+    openMenu();
+  });
+
+  menuItems.forEach(function (menuItem) {
+    menuItem.addEventListener("click", function (event) {
+      closeMenu();
     });
   });
-  
+});
