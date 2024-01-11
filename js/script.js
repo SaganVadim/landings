e56351c7c1c83156de7ac25b8bd9af7840032b4c
ant0n0vskyi <vad-y-one@mail.ru>
2024-01-11 15:53:44 +0300

    8

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerMenu = document.querySelector(".burger-menu");
+  const navLinks = document.querySelector(".nav-links");
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    const burgerMenu = document.querySelector('.burger-menu');
-    const navLinks = document.querySelector('.nav-links');
-  
-    burgerMenu.addEventListener('click', function () {
-      navLinks.classList.toggle('show');
-    });
-  
-    navLinks.addEventListener('click', function () {
-      navLinks.classList.remove('show');
-    });
+  burgerMenu.addEventListener("click", function () {
+    navLinks.classList.toggle("show");
   });
 
+  navLinks.addEventListener("click", function () {
+    navLinks.classList.remove("show");
+  });
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+  const rwgsegawToggles = document.querySelectorAll(".rwgsegaw-toggle");
 
-document.addEventListener("DOMContentLoaded", function() {
-    const dropdownToggles = document.querySelectorAll(".dropdown-toggle");
-  
-    dropdownToggles.forEach(function(toggle) {
-      toggle.addEventListener("click", function() {
-        const dropdownContent = this.nextElementSibling;
-        if (dropdownContent.style.height && dropdownContent.style.height !== "0px") {
-          dropdownContent.style.height = "0px";
-        } else {
-          dropdownContent.style.height = `${dropdownContent.scrollHeight}px`;
-        }
-      });
+  rwgsegawToggles.forEach(function (toggle) {
+    toggle.addEventListener("click", function () {
+      const rwgsegawContent = this.nextElementSibling;
+      if (
+        rwgsegawContent.style.height &&
+        rwgsegawContent.style.height !== "0px"
+      ) {
+        rwgsegawContent.style.height = "0px";
+      } else {
+        rwgsegawContent.style.height = `${rwgsegawContent.scrollHeight}px`;
+      }
     });
   });
-  
+});
