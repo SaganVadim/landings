27a000a15c128fc2f0f8186f5d87516771519087
ant0n0vskyi <vad-y-one@mail.ru>
2024-01-13 02:55:10 +0300

    3

--- js/js.js
+++ js/js.js
@@ -1,38 +1,40 @@
-document.querySelector('.burger').addEventListener('click', function(){
-    this.classList.toggle('active');
-    document.querySelector('.nav').classList.toggle('open');
+document.querySelector(".burger").addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.querySelector(".nav").classList.toggle("open");
 
-    document.body.style.overflow = (document.body.style.overflow === 'hidden') ? 'auto' : 'hidden';
+  document.body.style.overflow =
+    document.body.style.overflow === "hidden" ? "auto" : "hidden";
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const tabs = document.querySelectorAll(".tabs-tab-link");
+  const tabContents = document.querySelectorAll(".yrds");
 
-document.addEventListener('DOMContentLoaded', function () {
-    const tabs = document.querySelectorAll('.tabs-tab-link');
-    const tabContents = document.querySelectorAll('.w-tab-pane');
-  
-    tabs.forEach((tab, index) => {
-      tab.addEventListener('click', () => {
-        document.querySelector('.tabs-tab-link.w--current').classList.remove('w--current');
-        tab.classList.add('w--current');
-  
-        document.querySelector('.w-tab-pane.w--tab-active').classList.remove('w--tab-active');
-        tabContents[index].classList.add('w--tab-active');
-      });
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      document
+        .querySelector(".tabs-tab-link.sgvfdb")
+        .classList.remove("sgvfdb");
+      tab.classList.add("sgvfdb");
+
+      document.querySelector(".yrds.sgtrhsedg").classList.remove("sgtrhsedg");
+      tabContents[index].classList.add("sgtrhsedg");
     });
   });
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+  const faqs = document.querySelectorAll(".faqs-accordion");
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const faqs = document.querySelectorAll('.faqs-accordion');
+  faqs.forEach(function (faq) {
+    const question = faq.querySelector(".faqs-question");
+    const answer = faq.querySelector(".faqs-answer");
 
-    faqs.forEach(function (faq) {
-        const question = faq.querySelector('.faqs-question');
-        const answer = faq.querySelector('.faqs-answer');
+    question.addEventListener("click", function () {
+      answer.style.height =
+        answer.clientHeight === 0 ? `${answer.scrollHeight}px` : 0;
 
-        question.addEventListener('click', function () {
-            answer.style.height = answer.clientHeight === 0 ? `${answer.scrollHeight}px` : 0;
-
-            faq.classList.toggle('active');
-        });
+      faq.classList.toggle("active");
     });
+  });
 });
