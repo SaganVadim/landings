8d863d27454b8d102941cc1ed713d56c7f9407f6
ant0n0vskyi <vad-y-one@mail.ru>
2024-01-17 00:13:42 +0300

    pre rname

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 document.querySelector('.burger').addEventListener('click', function(){
   this.classList.toggle('active');
   document.querySelector('.nav').classList.toggle('open');
+
+  document.body.style.overflow =
+    document.body.style.overflow === "hidden" ? "auto" : "hidden";
 });
