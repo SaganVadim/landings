eec97654b9df201c70f454dcb234ca5e5620d1f8
ant0n0vskyi <vad-y-one@mail.ru>
2024-01-17 00:47:55 +0300

    ok

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-document.querySelector('.burger').addEventListener('click', function(){
-  this.classList.toggle('active');
-  document.querySelector('.nav').classList.toggle('open');
+document.querySelector(".erheh").addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.querySelector(".nav").classList.toggle("open");
 
   document.body.style.overflow =
     document.body.style.overflow === "hidden" ? "auto" : "hidden";
