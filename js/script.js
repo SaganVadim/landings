75357eef594b59bb4c1d355d4633f0cbb936caf7
ant0n0vskyi <vad-y-one@mail.ru>
2024-01-20 00:21:48 +0300

    800

--- js/script.js
+++ js/script.js
@@ -6,47 +6,41 @@ document.querySelector(".nfrnr").addEventListener("click", function () {
     document.body.style.overflow === "hidden" ? "auto" : "hidden";
 });
 
-function toggleFAQ(questionNumber) {
-    const faqQuestion = document.querySelector(`[data-w-id="${questionNumber}"]`);
-    const faqQuestionLower = faqQuestion.querySelector('.faq-question-lower');
-    
-    if (faqQuestionLower.style.height === '0px') {
-      faqQuestionLower.style.height = 'auto';
-    } else {
-      faqQuestionLower.style.height = '0px';
-    }
+function togglebnv(questionNumber) {
+  const bnvQuestion = document.querySelector(`[data-w-id="${questionNumber}"]`);
+  const bnvQuestionLower = bnvQuestion.querySelector(".bnv-question-lower");
+
+  if (bnvQuestionLower.style.height === "0px") {
+    bnvQuestionLower.style.height = "auto";
+  } else {
+    bnvQuestionLower.style.height = "0px";
   }
-  
-  document.querySelectorAll('.faq-question').forEach((question) => {
-    const questionNumber = question.getAttribute('data-w-id');
-    question.addEventListener('click', () => toggleFAQ(questionNumber));
-  });
-  
-   
-  
-  document.addEventListener('DOMContentLoaded', function () {
-    const accordionContents = document.querySelectorAll('.accordion-content');
-  
-    accordionContents.forEach((content) => {
-      const accordionUpper = content.querySelector('.accordion-upper');
-      const accordionLower = content.querySelector('.accordion-lower');
-  
-      accordionUpper.addEventListener('click', function () {
-        if (accordionLower.style.height === '0px' || accordionLower.style.height === '') {
-          accordionLower.style.height = `${accordionLower.scrollHeight}px`;
-        } else {
-          accordionLower.style.height = '0px';
-        }
-  
-        const arrowIcon = accordionUpper.querySelector('.accordion-arrow');
-        arrowIcon.classList.toggle('rotate-arrow');
-      });
+}
+
+document.querySelectorAll(".bnv-question").forEach((question) => {
+  const questionNumber = question.getAttribute("data-w-id");
+  question.addEventListener("click", () => togglebnv(questionNumber));
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionContents = document.querySelectorAll(".accordion-content");
+
+  accordionContents.forEach((content) => {
+    const accordionUpper = content.querySelector(".accordion-upper");
+    const accordionLower = content.querySelector(".accordion-lower");
+
+    accordionUpper.addEventListener("click", function () {
+      if (
+        accordionLower.style.height === "0px" ||
+        accordionLower.style.height === ""
+      ) {
+        accordionLower.style.height = `${accordionLower.scrollHeight}px`;
+      } else {
+        accordionLower.style.height = "0px";
+      }
+
+      const arrowIcon = accordionUpper.querySelector(".accordion-arrow");
+      arrowIcon.classList.toggle("rotate-arrow");
     });
   });
-  
-
-
-
-
-
-
+});
