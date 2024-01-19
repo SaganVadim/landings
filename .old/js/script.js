cf72daef48179ef8f385901dabe8ea65ecd717d7
ant0n0vskyi <vad-y-one@mail.ru>
2024-01-19 23:59:45 +0300

    300

--- .old/js/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".nfrnr").addEventListener("click", function () {
-  this.classList.toggle("active");
-  document.querySelector(".nav").classList.toggle("open");
-
-  document.body.style.overflow =
-    document.body.style.overflow === "hidden" ? "auto" : "hidden";
-});
