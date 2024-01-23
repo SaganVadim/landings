3091189da665be05a2b6810839ae2fe7e2fc68a8
ant0n0vskyi <vad-y-one@mail.ru>
2024-01-24 00:57:54 +0300

    ok

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
 document.querySelector(".nfrnr").addEventListener("click", function () {
-    this.classList.toggle("active");
-    document.querySelector(".nav").classList.toggle("open");
-  
-    document.body.style.overflow =
-      document.body.style.overflow === "hidden" ? "auto" : "hidden";
+  this.classList.toggle("active");
+  document.querySelector(".nav").classList.toggle("open");
+
+  document.body.style.overflow =
+    document.body.style.overflow === "hidden" ? "auto" : "hidden";
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var questionElements = document.querySelectorAll(".ewqfwq");
+
+  questionElements.forEach(function (question) {
+    question.addEventListener("click", toggleAnswer);
   });
 
+  function toggleAnswer(event) {
+    var parentElement = event.currentTarget.closest(".wvgwvb");
 
-document.addEventListener('DOMContentLoaded', function () {
-    var questionElements = document.querySelectorAll('.faq-question');
+    var answerElement = parentElement.querySelector(".bngtr");
 
-    questionElements.forEach(function (question) {
-      question.addEventListener('click', toggleAnswer);
-    });
-
-    function toggleAnswer(event) {
-      var parentElement = event.currentTarget.closest('.faq-item');
-
-      var answerElement = parentElement.querySelector('.faq-answer');
-
-      if (answerElement.style.height === '0px' || answerElement.style.height === '') {
-        answerElement.style.height = answerElement.scrollHeight + 'px';
-      } else {
-        answerElement.style.height = '0px';
-      }
+    if (
+      answerElement.style.height === "0px" ||
+      answerElement.style.height === ""
+    ) {
+      answerElement.style.height = answerElement.scrollHeight + "px";
+    } else {
+      answerElement.style.height = "0px";
     }
-  });
+  }
+});
