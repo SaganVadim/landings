c25b86fa9dd41d1df275d1350dbe5b1986d61084
ant0n0vskyi <vad-y-one@mail.ru>
2024-01-30 13:40:06 +0300

    ok

--- script2.js
+++ script2.js
@@ -1,10 +1,10 @@
-document.querySelector(".nfrnr").addEventListener("click", function () {
+
+  
+document.querySelector(".nfrnr2").addEventListener("click", function () {
     this.classList.toggle("active");
     document.querySelector(".nav").classList.toggle("open");
   
     document.body.style.overflow =
       document.body.style.overflow === "hidden" ? "auto" : "hidden";
   });
-
-
   
