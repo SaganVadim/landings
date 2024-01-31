987baed0d5592507a35ee13e225c4b3904df4020
ant0n0vskyi <vad-y-one@mail.ru>
2024-02-01 02:28:00 +0300

    1

---
+++ .old/script.js
@@ -0,0 +1,10 @@
+document.querySelector(".nfrnr").addEventListener("click", function () {
+    this.classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("open");
+  
+    document.body.style.overflow =
+      document.body.style.overflow === "hidden" ? "auto" : "hidden";
+  });
+
+
+  
