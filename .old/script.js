fb1682efc942e6632f324f68d3eec46bd200a1ae
ant0n0vskyi <vad-y-one@mail.ru>
2024-04-12 10:21:46 +0300

    init

--- .old/script.js
+++ .old/script.js
@@ -1,6 +1,6 @@
-document.querySelector(".nfrnr").addEventListener("click", function () {
+document.querySelector(".nFwUCswWCW").addEventListener("click", function () {
     this.classList.toggle("active");
-    document.querySelector(".nav").classList.toggle("open");
+    document.querySelector(".rIEenSoLUM").classList.toggle("open");
   
     document.body.style.overflow =
       document.body.style.overflow === "hidden" ? "auto" : "hidden";
