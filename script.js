4266f5a8f3f471a51d8d2da46d588ae3437da371
ant0n0vskyi <vad-y-one@mail.ru>
2024-05-10 14:48:09 +0300

    ok

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.querySelector(".cVedaERpuW").addEventListener("click", function () {
+document.querySelector(".nFwUCswWCW").addEventListener("click", function () {
     this.classList.toggle("active");
-    document.querySelector(".lXkBkzXeOj").classList.toggle("open");
+    document.querySelector(".rIEenSoLUM").classList.toggle("open");
   
     document.body.style.overflow =
       document.body.style.overflow === "hidden" ? "auto" : "hidden";
