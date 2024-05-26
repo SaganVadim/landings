f2e4cd8f6796994061b38da0431f19bdba15666e
ant0n0vskyi <vad-y-one@mail.ru>
2024-05-26 23:42:06 +0300

    init

---
+++ .old/script.js
@@ -0,0 +1,10 @@
+document.querySelector(".nFwUCswWCW").addEventListener("click", function () {
+    this.classList.toggle("active");
+    document.querySelector(".rIEenSoLUM").classList.toggle("open");
+  
+    document.body.style.overflow =
+      document.body.style.overflow === "hidden" ? "auto" : "hidden";
+  });
+
+
+  
