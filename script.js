dc0c8cb65fcf9ad7737c80b6022ef629c0e5951d
ant0n0vskyi <vad-y-one@mail.ru>
2024-08-03 10:05:22 +0300

    ok

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-
 const toggler = document.querySelector(".header-toggler");
 const menu = document.querySelector(".header-menu");
 const menuItems = document.querySelectorAll(".header-menu__item");
+const html = document.querySelector("html");
 
 toggler.addEventListener("click", () => {
   menu.classList.toggle("header-menu--open");
   toggler.classList.toggle("header-toggler--open");
+  html.classList.toggle("unscroll");
 });
 
 menuItems.forEach((e) => {
   e.addEventListener("click", () => {
     menu.classList.remove("header-menu--open");
     toggler.classList.remove("header-toggler--open");
+    html.classList.remove("unscroll"); // Удаление класса unscroll
   });
 });
-
-let html = document.querySelector("html");
-document.querySelector(".header-toggler").onclick = function () {
-  html.classList.toggle("unscroll");
-};
