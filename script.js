647c430f6532a20d1e90b0eccd3e2dc13e294a11
ant0n0vskyi <vad-y-one@mail.ru>
2024-09-24 21:41:56 +0300

    ok

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-const toggler = document.querySelector(".header-toggler");
-const menu = document.querySelector(".header-menu");
-const menuItems = document.querySelectorAll(".header-menu__item");
-const html = document.querySelector("html");
-
-toggler.addEventListener("click", () => {
-  menu.classList.toggle("header-menu--open");
-  toggler.classList.toggle("header-toggler--open");
-  html.classList.toggle("unscroll"); 
-});
-
-menuItems.forEach(item => {
-  item.addEventListener("click", () => {
-    menu.classList.remove("header-menu--open");
-    toggler.classList.remove("header-toggler--open");
-    html.classList.remove("unscroll"); 
+document.querySelector(".cVedaERpuW").addEventListener("click", function () {
+    this.classList.toggle("active");
+    document.querySelector(".lXkBkzXeOj").classList.toggle("open");
+  
+    document.body.style.overflow =
+      document.body.style.overflow === "hidden" ? "auto" : "hidden";
   });
-});
+
+
+  
