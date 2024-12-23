548d89324837074f61641925496693fde6c4ebc4
ant0n0vskyi <vad-y-one@mail.ru>
2024-12-23 21:20:10 +0300

    ok

--- .old/wow.js
+++ .old/wow.js
@@ -458,8 +458,6 @@
     }, {
       key: 'cacheAnimationName',
       value: function cacheAnimationName(box) {
-        // https://bugzilla.mozilla.org/show_bug.cgi?id=921834
-        // box.dataset is not supported for SVG elements in Firefox
         return this.animationNameCache.set(box, this.animationName(box));
       }
     }, {
