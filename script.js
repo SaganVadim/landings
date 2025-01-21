b5494c3f0ddd7dcd0ac303237b51f39348fa5a35
ant0n0vskyi <vad-y-one@mail.ru>
2025-01-21 23:00:33 +0300

    ok

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function toggleAccordion(element) {
-    const answerDiv = element.querySelector('.JNnXdbqDLg');
+    const answerDiv = element.querySelector('.rimshots');
 
    
     if (answerDiv.classList.contains('open')) {
         answerDiv.classList.remove('open'); 
     } else {
         
-        const allAnswers = document.querySelectorAll('.JNnXdbqDLg');
+        const allAnswers = document.querySelectorAll('.rimshots');
         allAnswers.forEach(answer => {
             answer.classList.remove('open'); 
         });
@@ -28,22 +28,3 @@ function isInViewport(element) {
 
 
 
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-const paragraphElements = document.querySelectorAll('.bqOIKmLmbp');
-
-window.addEventListener('scroll', function () {
-    paragraphElements.forEach((p) => {
-        if (isInViewport(p)) {
-            p.classList.add('scrolled');
-        }
-    });
-});
