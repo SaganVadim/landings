84efb236e873c7dd64bede067601fbc4f9cf2653
ant0n0vskyi <vad-y-one@mail.ru>
2025-01-21 20:10:25 +0300

    ok

---
+++ script.js
@@ -0,0 +1,49 @@
+function toggleAccordion(element) {
+    const answerDiv = element.querySelector('.JNnXdbqDLg');
+
+   
+    if (answerDiv.classList.contains('open')) {
+        answerDiv.classList.remove('open'); 
+    } else {
+        
+        const allAnswers = document.querySelectorAll('.JNnXdbqDLg');
+        allAnswers.forEach(answer => {
+            answer.classList.remove('open'); 
+        });
+        answerDiv.classList.add('open'); 
+    }
+}
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+
+
+
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+const paragraphElements = document.querySelectorAll('.bqOIKmLmbp');
+
+window.addEventListener('scroll', function () {
+    paragraphElements.forEach((p) => {
+        if (isInViewport(p)) {
+            p.classList.add('scrolled');
+        }
+    });
+});
